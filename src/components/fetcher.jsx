06feec38f8b76e835f05e0b3dc1774bfd130cd75
Eduard Ortegon <eduardortegon2398@gmail.com>
2024-09-12 22:45:23 -0500

    Adding filters, by status, gender and species also by name.

--- src/components/fetcher.jsx
+++ src/components/fetcher.jsx
@@ -5,6 +5,9 @@ const fetcher = () => {
   const [characters, setCharacters] = useState([]);
   const [filteredCharacters, setFilteredCharacters] = useState([]); // Datos filtrados
   const [searchTerm, setSearchTerm] = useState(""); // Término de búsqueda
+  const [statusFilter, setStatusFilter] = useState("");
+  const [speciesFilter, setSpeciesFilter] = useState("");
+  const [genderFilter, setGenderFilter] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -12,7 +15,7 @@ const fetcher = () => {
     const fetchCharacters = async () => {
       try {
         const response = await fetch(
-          "https://rickandmortyapi.com/api/character/?page=19"
+          "https://rickandmortyapi.com/api/character/?page=18"
         );
         if (!response.ok) {
           throw new Error("Error fetching data");
@@ -30,71 +33,122 @@ const fetcher = () => {
     fetchCharacters();
   }, []);
 
-  const handleSearch = (e) => {
-    const value = e.target.value.toLowerCase(); 
-    setSearchTerm(value);
+  const handleSearch = () => {
+    let filtered = characters;
 
-    
-    const filtered = characters.filter((character) =>// Filtrar los personajes según el término de búsqueda
-      character.name.toLowerCase().includes(value)
-    );
+    if (searchTerm) {
+      filtered = filtered.filter((character) =>
+        character.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+
+    if (statusFilter) {
+      filtered = filtered.filter(
+        (character) =>
+          character.status.toLowerCase() === statusFilter.toLowerCase()
+      );
+    }
+
+    if (speciesFilter) {
+      filtered = filtered.filter(
+        (character) =>
+          character.species.toLowerCase() === speciesFilter.toLowerCase()
+      );
+    }
+
+    if (genderFilter) {
+      filtered = filtered.filter(
+        (character) =>
+          character.gender.toLowerCase() === genderFilter.toLowerCase()
+      );
+    }
+
     setFilteredCharacters(filtered);
   };
 
+  useEffect(() => {
+    handleSearch();
+  }, [searchTerm, statusFilter, speciesFilter, genderFilter]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div>
-      <h1 className="mt-10 text-green-600 text-3xl font-bold text-center mb-8 font-poppins">
+    <div className="container mx-auto">
+      <h1 className="mt-10 text-green-600 text-3xl font-bold text-center mb-8">
         Rick and Morty Characters
       </h1>
 
       {/* Input de búsqueda */}
-      <div className="mb-8 text-center">
-  <input
-    type="text"
-    placeholder="Search for a character..."
-    value={searchTerm}
-    onChange={handleSearch}
-    className="w-full max-w-md px-4 py-2 border border-gray-300 rounded-full shadow-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition duration-300 ease-in-out transform hover:scale-105 bg-white text-gray-700 placeholder-gray-400"
-  />
-</div>
+      <div className="flex flex-col md:flex-row justify-between mb-6 space-y-4 md:space-y-0 md:space-x-4">
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full max-w-xs px-4 py-2 border border-gray-300 rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+        />
 
+        {/* Filtro por status */}
+        <select
+          value={statusFilter}
+          onChange={(e) => setStatusFilter(e.target.value)}
+          className="w-full max-w-xs px-4 py-2 border border-gray-300 rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+        >
+          <option value="">Filter by Status</option>
+          <option value="alive">Alive</option>
+          <option value="dead">Dead</option>
+          <option value="unknown">Unknown</option>
+        </select>
+
+        {/* Filtro por especie */}
+        <select
+          value={speciesFilter}
+          onChange={(e) => setSpeciesFilter(e.target.value)}
+          className="w-full max-w-xs px-4 py-2 border border-gray-300 rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+        >
+          <option value="">Filter by Species</option>
+          <option value="human">Human</option>
+          <option value="alien">Alien</option>
+        </select>
+
+        {/* Filtro por género */}
+        <select
+          value={genderFilter}
+          onChange={(e) => setGenderFilter(e.target.value)}
+          className="w-full max-w-xs px-4 py-2 border border-gray-300 rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+        >
+          <option value="">Filter by Gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="genderless">Genderless</option>
+          <option value="unknown">Unknown</option>
+        </select>
+      </div>
 
       {/* Lista de personajes filtrados */}
-      <ul className="p-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-6">
-        {filteredCharacters.length > 0 ? filteredCharacters.map((character) => (
-          <li
-            className="bg-white shadow-lg rounded-lg overflow-hidden border border-gray-200 hover:shadow-xl transition-shadow duration-300 font-poppins"
-            key={character.id}
-          >
-            <div className="relative w-full h-64">
-              <img
-                src={character.image}
-                alt={character.name}
-                className="w-full h-full object-cover"
-              />
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        {filteredCharacters.map((character) => (
+          <li key={character.id} className="bg-white shadow-lg rounded-lg p-4">
+            <img
+              src={character.image}
+              alt={character.name}
+              className="w-full h-64 object-cover rounded-t-lg"
+            />
+            <div className="text-center mt-4">
+              <p className="text-xl font-bold">{character.name}</p>
+              <p className="text-gray-600">Species: {character.species}</p>
             </div>
-            <div className="p-4 text-center">
-              <p className="text-lg font-semibold text-gray-800 mb-1">
-                {character.name === "Two Guys with Handlebar Mustaches"? "Two Guys with Handlebar" :character.name}
-              </p>
-              <p className="text-gray-600 text-sm">Species: {character.species}</p>
-            </div>
-            <div className="flex justify-between items-center p-4 border-t border-gray-300">
-              <button className="w-full flex items-center justify-center text-red-500 hover:text-red-600 transition-colors">
-                <FiTrash className="mr-2" />
-                <span>Delete</span>
+            <div className="flex justify-between mt-4">
+              <button className="flex items-center text-red-500 hover:text-red-600">
+                <FiTrash className="mr-2" /> Delete
               </button>
-              <button className="w-full flex items-center justify-center text-blue-500 hover:text-blue-600 transition-colors">
-                <FiInfo className="mr-2" />
-                <span>Details</span>
+              <button className="flex items-center text-blue-500 hover:text-blue-600">
+                <FiInfo className="mr-2" /> Details
               </button>
             </div>
           </li>
-        )):<p className="text-center bg-green-500 text-white">No se han encontrado coincidencias con la busqueda introducida.</p>}
+        ))}
       </ul>
     </div>
   );
